f2106b31c1c6a3d6c276bd5809f87db062c5346f
guillenvd <guillenvd@gmail.com>
2015-12-17 18:14:48 -0600

    UI

---
+++ www/js/controller.js
@@ -0,0 +1,17 @@
+var menu = {
+			signin: function(){
+					location.href = "signin.html";
+				},
+			signup:function() {
+				    location.href = "signup.html";
+				},
+			resetPassword:function(){
+					location.href = "resetPassword.html";
+				},
+			tour:function() {
+					console.log();
+				},
+			success:function(){
+					location.href = "success.html";
+				}
+			};
