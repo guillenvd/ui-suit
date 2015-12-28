8e85fc3d5dde5beec2c444df553d5e32e8d39029
David Guillen <davidguillen@Davids-Mac.local>
2015-12-28 11:27:28 -0800

    UI Icons

---
+++ platforms/ios/www/js/controller.js
@@ -0,0 +1,21 @@
+var menu = {
+			signin: function(){
+					location.href = "signin.html";
+				},
+			signup:function() {
+				    location.href = "signup.html";
+				},
+			resetPassword:function(){
+					location.href = "resetPassword.html";
+				},
+			tour:function() {
+					console.log();
+				},
+			success:function(){
+					location.href = "success.html";
+				},
+			listngAgent:function(){
+					location.href = "listingAgent.html";
+					}
+			
+			};
