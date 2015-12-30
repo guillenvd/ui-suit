5b77a1059bac7b62f7e0e7c0f667a8339693a196
David Guillen <davidguillen@Davids-Mac.local>
2015-12-29 17:42:50 -0800

    Design

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -5,13 +5,21 @@ module.exports = [
         "id": "cordova-plugin-whitelist.whitelist",
         "pluginId": "cordova-plugin-whitelist",
         "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "id": "cordova-plugin-statusbar.statusbar",
+        "pluginId": "cordova-plugin-statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "cordova-plugin-whitelist": "1.2.0",
-    "cordova-plugin-crosswalk-webview": "1.0.0"
+    "cordova-plugin-statusbar": "2.0.0"
 }
 // BOTTOM OF METADATA
 });
